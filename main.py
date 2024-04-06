391353283f47b4fb05b6735dcf36db8f87cb992d
abhianv <abhianvkallungal15@gmail.com>
2024-04-06 17:12:58 +0530

    login ,signup and logout is done

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
-import re
-from sqlite3 import adapters
-from fastapi import FastAPI,Request
+from fastapi import FastAPI, Request, Response, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -17,13 +15,24 @@ app.mount('/static',StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
 
 
+@app.get("/signup",response_class=HTMLResponse)
+async def signup_read_root(request:Request):
+    return templates.TemplateResponse('signup.html',{'request' : request,})
+
+
+@app.get("/login", response_class=HTMLResponse)
+async def login_page(request: Request):
+    return templates.TemplateResponse('login.html',{'request' : request,})
+
+
+
+
 @app.get("/",response_class=HTMLResponse)
 async def read_root(request:Request): 
 
     id_token = request.cookies.get('token')
     error_message = 'No Error Here'
     user_token = None
-
     if id_token:
         try:
             user_token =google.oauth2.id_token.verify_firebase_token(id_token,firebase_request_adapter)
@@ -32,9 +41,13 @@ async def read_root(request:Request):
 
     return templates.TemplateResponse('main.html',{'request' : request, 'user_token':user_token,'error_message':error_message})
 
-@app.get("/login",response_class=HTMLResponse)
-async def login_read_root(request:Request):
-    return templates.TemplateResponse('login.html',{'request' : request, 'name':'jhone doe','number':'1234241'})
+# @app.get("/login",response_class=HTMLResponse)
+# async def login_read_root(request:Request):
+#     return templates.TemplateResponse('login.html',{'request' : request, 'name':'jhone doe','number':'1234241'})
+
+
+
+
 
 @app.get("/signup",response_class=HTMLResponse)
 async def signup_read_root(request:Request):
